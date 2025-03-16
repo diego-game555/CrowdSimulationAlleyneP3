ea3e583d1226f542ea84d1365712d3a7be527c3e
Diego Alleyne <145381036+diego-game555@users.noreply.github.com>
2025-03-16 13:50:02 -0700

    Finishing up
    Finishing up the project 3/16

--- CrowdSimulationP3Alleyne/Assets/Flock.cs
+++ CrowdSimulationP3Alleyne/Assets/Flock.cs
@@ -6,6 +6,7 @@ public class Flock : MonoBehaviour
 {
     public FlockManager myManager;
     float speed;
+    bool turning = false;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,37 @@ public class Flock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ApplyRules();
+        Bounds b = new Bounds(myManager.transform.position, myManager.swimLimits * 2);
+
+        RaycastHit hit = new RaycastHit();
+        Vector3 direction = Vector3.zero;
+
+        if (!b.Contains(transform.position)) 
+        {
+            turning = true;
+            direction = myManager.transform.position - transform.position;
+        } 
+        else if (Physics.Raycast(transform.position, this.transform.forward * 50, out hit))
+        {
+            turning = true;
+            direction = Vector3.Reflect(this.transform.forward, hit.normal);
+        }
+        else
+            turning = false;
+
+        if (turning)
+        {
+            
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), myManager.rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if (Random.Range(0, 100) < 10)
+                speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+            if(Random.Range(0,100) < 20)
+                ApplyRules();
+        }
+        
         transform.Translate(0, 0, Time.deltaTime * speed);
     }
 
@@ -54,7 +85,7 @@ public class Flock : MonoBehaviour
 
         if(groupSize > 0)
         {
-            vcentre = vcentre/groupSize;
+            vcentre = vcentre/groupSize + (myManager.goalPos - this.transform.position);
             speed = gSpeed/groupSize;
 
             Vector3 direction = (vcentre + vavoid) - transform.position;
